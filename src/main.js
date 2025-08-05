63f043f4f0638b4c57ee29d50b8d133f86fe99f7
jbelardino <jbelardino@gmail.com>
2025-08-05 02:02:15 -0400

    fixed the dragging bugs

--- src/main.js
+++ src/main.js
@@ -38,28 +38,32 @@ function initializeBoard() {
 };
 
 initializeBoard();
+document.addEventListener("dragstart", (e) => {
+  if (!e.target.classList.contains("piece")) {
+    e.preventDefault(); //Block unintended drag
+  }
+});
+
 const squares = document.querySelectorAll(".square");
 const pieces = document.querySelectorAll(".piece")
+let selectedPiece = null;
+let startPositionId = null;
 
-function setEventListeners() {
-    pieces.forEach((piece) => {
-        piece.addEventListener('dragstart', dragStart)
-        // piece.addEventListener('dragover', dragOver)
-        // piece.addEventListener('drop', dragDrop)
-    })
+pieces.forEach((piece) => {
+    piece.addEventListener('dragstart', dragStart)
+    // piece.addEventListener('dragover', dragOver)
+    // piece.addEventListener('drop', dragDrop)
+})
     
-    squares.forEach((square) => {
-        //square.addEventListener('dragstart', dragStart)
-        square.addEventListener('dragover', dragOver)
-        square.addEventListener('drop', dragDrop)
-    })
-};
+squares.forEach((square) => {
+    //square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
 
-let draggedElement
-let startPositionId
 function dragStart(e) {
     console.log("dragStart: ", e.target)
-    draggedElement = e.target
+    selectedPiece = e.target
     startPositionId = e.target.parentNode.getAttribute('id')
     
 }
@@ -73,19 +77,17 @@ function dragDrop(e) {
 
     const containsPiece = e.target.classList.contains('piece')
     if(containsPiece) {
-        e.target.parentNode.append(draggedElement)
+        e.target.parentNode.append(selectedPiece)
         e.target.remove()
     }
     else {
-        e.target.append(draggedElement)
+        e.target.append(selectedPiece)
     }
 
     console.log("dragDrop: ", e.target)
-    
-
+    selectedPiece = null;
 }
 
-setEventListeners();
 
 
 
